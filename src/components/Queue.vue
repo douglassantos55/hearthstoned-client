a5971f9be82dc75715b44d4a81ee09de30260bf9
Douglas Santos <hiperhavy_doug@hotmail.com>
2022-06-18 23:11:08 -0300

    feat: queue component

---
+++ src/components/Queue.vue
@@ -0,0 +1,41 @@
+<script lang="ts">
+import { ref } from 'vue'
+import server from '../server'
+
+export default {
+    name: 'Queue',
+    setup() {
+        const matchId = ref('')
+        const waiting = ref(false)
+
+        server.on('wait_for_match', () => waiting.value = true)
+        server.on('wait_other_players', () => waiting.value = true)
+
+        server.on('confirm_match', (match: string) => {
+            matchId.value = match
+            waiting.value = false
+        })
+
+        function queueUp() {
+            server.send('queue')
+        }
+
+        function confirm() {
+            server.send('match_confirmed', matchId.value)
+        }
+
+        function decline() {
+            server.send('match_declined', matchId.value)
+        }
+
+        return { matchId, waiting, queueUp, confirm, decline }
+    }
+}
+</script>
+
+<template>
+    <button @click="queueUp" v-if="!matchId" :disabled="waiting">Queue up</button>
+
+    <button @click="confirm" v-if="matchId" :disabled="waiting">Confirm match</button>
+    <button @click="cancel" v-if="matchId" :disabled="waiting">Cancel match</button>
+</template>
---
+++ src/server.ts
@@ -0,0 +1,52 @@
+const conn = new WebSocket('ws://0.0.0.0:8080')
+
+type Listener = (payload: any) => void
+
+const listeners: { [type: string]: Listener[] } = {}
+
+const server = {
+    /**
+     * Sends an event to server
+     *
+     * @param {string} type - The event type
+     * @param {any} payload - The payload to send
+     */
+    send(type: string, payload: any = null) {
+        conn.send(JSON.stringify({ type, payload }))
+    },
+
+    /**
+     * Registers a callback for server responses
+     *
+     * @param {string} type - The event type to watch
+     * @param {function} listener - The callback to execute when the event is received from server
+     */
+    on(type: string, listener: Listener) {
+        if (!listeners[type]) {
+            listeners[type] = []
+        }
+        listeners[type].push(listener)
+    },
+
+    /**
+     * Executes all registered listeners for a given event type
+     *
+     * @param {string} type - The event type received from server
+     * @param {object} payload - The payload received from server
+     */
+    _notify(type: string, payload: Record<string, any>) {
+        for (const listener of listeners[type]) {
+            listener(payload)
+        }
+    }
+}
+
+// Notifies listeners when messages are received from server
+conn.onmessage = function(message: MessageEvent) {
+    const data = JSON.parse(message.data)
+    if (data) {
+        server._notify(data.type, data.payload)
+    }
+}
+
+export default server
